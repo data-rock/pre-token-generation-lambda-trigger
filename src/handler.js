988415841ab7b3c684f7f3c1f9d88f2ab5a2440a
Mario Martinez <zenitram.oiram@gmail.com>
2021-07-23 11:08:36 +0800

    set up babel/eslint/prettier

---
+++ src/handler.js
@@ -0,0 +1,14 @@
+module.exports.preTokenGeneration = (event, context, callback) => {
+  event.response = {
+    claimsOverrideDetails: {
+      claimsToAddOrOverride: {
+        attribute_key2: 'attribute_value2',
+        attribute_key: 'attribute_value',
+      },
+      claimsToSuppress: ['email'],
+    },
+  };
+
+  // Return to Amazon Cognito
+  callback(null, event);
+};
