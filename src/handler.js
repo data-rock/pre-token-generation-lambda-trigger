f27e793b6053fe405bcdf91e8402bb2706093fe8
Mario Martinez <zenitram.oiram@gmail.com>
2021-07-26 14:55:48 +0800

    Rewrite logic without ternaries

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,22 @@
 import { getCompany } from './imageMetadataApi';
 
+const isMfaRequired = async (isAdmin, companyId) => {
+  if (isAdmin) return true;
+  if (companyId) {
+    const company = await getCompany(companyId);
+    return company.mfaRequired;
+  }
+  return false;
+};
+
 export const preTokenGeneration = async (event, context, callback) => {
   console.log('event :', event);
-  const isAdmin = !!event.request.userAttributes['custom:isAdmin'];
+  const isAdmin = event.request.userAttributes['custom:isAdmin'];
   console.log('isAdmin :', isAdmin);
   const companyId = event.request.userAttributes['custom:companyId'];
   console.log('companyId :', companyId);
 
-  const mfaRequired = isAdmin || companyId ? (await getCompany(companyId)).mfaRequired : false;
+  const mfaRequired = await isMfaRequired(isAdmin, companyId);
   console.log('mfaRequired :', mfaRequired);
 
   event.response = {
