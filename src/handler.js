4ca346cee697e33cdc06541133c82eb6cfa28bd0
Mario Martinez <zenitram.oiram@gmail.com>
2021-07-27 12:07:16 +0800

    Add MFA claims (#2)
    * Retrieve mfaRequired from image-metadata-api
    
    * await result of getCompany
    
    * Rewrite logic without ternaries
    
    * Retrieve user via aws-sdk
    
    * Give lambda permissions for AdminGetUser
    
    * Use more general resource name
    
    * Revert ssm
    
    * Use Fn::Sub
    
    * Feature complete
    
    * Add tests
    
    * Remove smoke test
    
    * Lock down specific user pool

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,31 @@
-export const preTokenGeneration = (event, context, callback) => {
+import { getUser } from './cognito';
+import { getCompany } from './imageMetadataApi';
+
+const isMfaRequired = async (isAdmin, companyId) => {
+  if (isAdmin) return true;
+  if (companyId) {
+    const company = await getCompany(companyId);
+    return company.mfaRequired;
+  }
+  return false;
+};
+
+export const preTokenGeneration = async (event, context, callback) => {
+  const isAdmin = event.request.userAttributes['custom:isAdmin'];
+  const companyId = event.request.userAttributes['custom:companyId'];
+  const mfaRequired = await isMfaRequired(isAdmin, companyId);
+
+  let mfaEnabled = false;
+  if (mfaRequired) {
+    const user = await getUser(event.userName);
+    mfaEnabled = user.PreferredMfaSetting === 'SOFTWARE_TOKEN_MFA';
+  }
+
   event.response = {
     claimsOverrideDetails: {
       claimsToAddOrOverride: {
-        attribute_key2: 'attribute_value2',
-        attribute_key: 'attribute_value',
+        mfaRequired,
+        mfaEnabled,
       },
     },
   };
---
+++ src/imageMetadataApi.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch';
+import config from './config';
+
+export const getCompany = async (id) => {
+  const fetchOptions = {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `digest ${config().metadataApi.key}`,
+    },
+  };
+  const response = await fetch(`${config().metadataApi.companyUrl}/${id}`, fetchOptions);
+
+  if (!response.ok) throw new Error(`Failed to retrieve company info for id: ${id}`);
+
+  return response.json();
+};
